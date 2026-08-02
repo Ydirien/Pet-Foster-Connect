01eae59940afef97adf775bbdb3df60469378204
gaëtan plassard <gaetan.plassard@hotmail.fr>
2026-08-02 02:21:06 +0200

    feat: auth.controller schema password

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -7,3 +7,9 @@ import { config } from "../../config.ts";
 import { ConflictError, UnauthorizedError } from "../lib/errors.ts";
 import { generateAccessToken,generateRefreshToken,generateResetPasswordToken,setAccessTokenCookie,setRefreshTokenCookie ,type Token } from "../lib/tokens.ts";
 
+const passwordSchema = z
+    .string()
+    .min(12, "Le mot de passe doit contenir au moins 12 caractères")
+    .regex(/[a-z]/, "Le mot de passe doit contenir au moins une minuscule")
+    .regex(/[A-Z]/, "Le mot de passe doit contenir au moins une majuscule")
+    .regex(/[0-9]/, "Le mot de passe doit contenir au moins un chiffre");
