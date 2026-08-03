11256b1b8368033a165d1dce41e7d969c81131e7
gaëtan plassard <gaetan.plassard@hotmail.fr>
2026-08-03 14:22:41 +0200

    ajout du rate-limit sur les routes auth

--- api/src/routes/auth.routes.ts
+++ api/src/routes/auth.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import * as authController from "../controllers/auth.controller.ts";
 import { authenticate } from "../middlewares/auth.middleware.ts";
+import { rateLimiters } from "../middlewares/rate-limit.middleware.ts";
 
 export const router = Router();
 
-router.post("/register", authController.registerUser);
-router.post("/login", authController.loginUser);
+router.post("/register", rateLimiters.register,authController.registerUser);
+router.post("/login", rateLimiters.login,authController.loginUser);
 router.post("/logout", authenticate, authController.logoutUser);
 router.get("/me", authenticate, authController.getAuthenticatedUser);
 router.post("/refresh", authController.refreshTokens);
-router.post("/forgot-password", authController.forgotPassword);
-router.post("/reset-password", authController.resetPassword);
+router.post("/forgot-password", rateLimiters.forgotPassword, authController.forgotPassword);
+router.post("/reset-password", rateLimiters.resetPassword, authController.resetPassword);
